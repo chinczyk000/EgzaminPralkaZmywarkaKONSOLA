84d34df0126bbac01050a11ca806c6048a57292e
chinczyk000 <tobiaszstojak@outlook.com>
2026-09-03 07:36:08 +0200

    Update Odkurzacz.java

--- src/Odkurzacz.java
+++ src/Odkurzacz.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class odkurzacz {
+public class Odkurzacz extends Urzadzenie{
+    private boolean czyOdkurzaczJestWlaczony = false;
+    public void on(){
+        if(czyOdkurzaczJestWlaczony == false){
+            this.czyOdkurzaczJestWlaczony = true;
+            wyswietlKomunikat("Odkurzacz włączono");
+        }else{
+            return;
+        }
+    }
+    public void off(){
+        if(czyOdkurzaczJestWlaczony == true){
+            this.czyOdkurzaczJestWlaczony = false;
+            wyswietlKomunikat("Odkurzacz wyłączono");
+        }else{
+            return;
+        }
+    }
 }
