364442c3b3e499f3c78f9d1403a801580823b529
chinczyk000 <tobiaszstojak@outlook.com>
2026-09-03 07:36:25 +0200

    Update Pralka.java

--- src/Pralka.java
+++ src/Pralka.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Pralka {
+public class Pralka extends Urzadzenie{
+    private int numerProgramuPrania = 0;
+
+    public int wybierzNumerProgramuPrania(int numerProgramuPrania) {
+        if(numerProgramuPrania < 1 || numerProgramuPrania > 12){
+            wyswietlKomunikat("Niepoprawny numer programu");
+        }else{
+            this.numerProgramuPrania = numerProgramuPrania;
+        }
+        return numerProgramuPrania;
+    }
 }
