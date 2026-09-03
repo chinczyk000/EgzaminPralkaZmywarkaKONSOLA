e3a6de8b7af9d786de8e64766b284ec2378de1f8
chinczyk000 <tobiaszstojak@outlook.com>
2026-09-03 07:36:53 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pralka pralka = new Pralka();
+        Odkurzacz odkurzacz = new Odkurzacz();
+
+        Scanner wyczytajLinijke = new Scanner(System.in);
+        System.out.println("Podaj numer prania 1...12");
+        int wybranyNumerPrania = wyczytajLinijke.nextInt();
+
+        System.out.println(pralka.wybierzNumerProgramuPrania(wybranyNumerPrania));
+
+
+        odkurzacz.on();
+        odkurzacz.on();
+        odkurzacz.on();
+        odkurzacz.wyswietlKomunikat("Odkurzacz wyladowal sie");
+        odkurzacz.off();
     }
 }
